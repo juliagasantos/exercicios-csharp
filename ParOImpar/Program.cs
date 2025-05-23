f5ab10d348fbe6386bf83672f2ff79e1fe32423c
juliagasantos <juliagalvaoalmeidasantos@gmail.com>
2025-05-23 20:31:26 -0300

    Adicionar arquivos de projeto.

---
+++ ParOImpar/Program.cs
@@ -0,0 +1,13 @@
+﻿var n1 = 0;
+
+Console.WriteLine("Digite um número: ");
+n1 = int.Parse(Console.ReadLine());
+
+if (n1 % 2 == 0)
+{
+    Console.WriteLine("O número é par.");
+}
+else
+{
+    Console.WriteLine("O número é ímpar.");
+}
