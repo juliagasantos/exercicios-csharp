2d4b49d8dcddd706acc4e4244f69a7f8dcd392a0
juliagasantos <juliagalvaoalmeidasantos@gmail.com>
2025-05-23 20:51:20 -0300

    exercicio - aprovado e reprovado

---
+++ AprovadoReprovado/Program.cs
@@ -0,0 +1,17 @@
+﻿var n = 0;
+
+Console.WriteLine("Digite uma nota: ");
+n = int.Parse(Console.ReadLine());
+
+if (n >= 7)
+{
+    Console.WriteLine("Aprovado!");
+}
+else if (n >= 5)
+{
+    Console.WriteLine("Recuperação!");
+}
+else
+{
+    Console.WriteLine("Reprovado!");
+}
