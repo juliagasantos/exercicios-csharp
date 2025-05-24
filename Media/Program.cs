f98b9768b520d02793908eb992524f3f962cdd1b
juliagasantos <juliagalvaoalmeidasantos@gmail.com>
2025-05-23 21:43:57 -0300

    exercicio - calculando média

---
+++ Media/Program.cs
@@ -0,0 +1,12 @@
+﻿double n1 = 0;
+double n2 = 0;
+double media = 0;
+
+Console.WriteLine("Digite um número:");
+n1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Digite outro número:");
+n2 = double.Parse(Console.ReadLine());
+
+media = (n1 + n2) / 2;
+
+Console.WriteLine($"A média é: {media}");
