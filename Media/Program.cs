a73a0f336435ad85c2c3868e66b02ae00cc6cb08
juliagasantos <juliagalvaoalmeidasantos@gmail.com>
2025-05-23 22:17:37 -0300

    alterações no exercicio "media".  - exibir se o aluna foi aprovado, está de recuperação ou foi reprovado

--- Media/Program.cs
+++ Media/Program.cs
@@ -2,11 +2,24 @@
 double n2 = 0;
 double media = 0;
 
-Console.WriteLine("Digite um número:");
+Console.WriteLine("Digite uma nota:");
 n1 = double.Parse(Console.ReadLine());
-Console.WriteLine("Digite outro número:");
+Console.WriteLine("Digite outra nota:");
 n2 = double.Parse(Console.ReadLine());
 
 media = (n1 + n2) / 2;
 
 Console.WriteLine($"A média é: {media}");
+
+if (media >= 7)
+{
+    Console.WriteLine("Aprovado");
+}
+else if (media >= 5)
+{
+    Console.WriteLine("Recuperação");
+}
+else
+{
+    Console.WriteLine("Reprovado");
+}
